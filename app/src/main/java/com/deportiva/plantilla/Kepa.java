3a866cd77b20d41dba2ab6a4b312d020a2703b21
David <dgomer00@estudiantes.unileon.es>
2015-04-13 01:04:50 +0200

    Método cargar para el jugador Kepa

--- app/src/main/java/com/deportiva/plantilla/Kepa.java
+++ app/src/main/java/com/deportiva/plantilla/Kepa.java
@@ -1,15 +1,21 @@
 package com.deportiva.plantilla;
 
 import com.deportiva.ayuda.AcercaDe;
+import com.deportiva.database.BaseDatosOpenHelper;
 import com.example.deportiva.main.R;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Kepa extends ActionBarActivity {
 
@@ -19,6 +25,10 @@ public class Kepa extends ActionBarActivity {
 		setContentView(R.layout.activity_kepa);
 		//Para que no se gire la pantalla al poner el movil en horizontal
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        //Cargamos los datos
+        cargar();
 	}
 
 	@Override
@@ -42,4 +52,49 @@ public class Kepa extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+    public void cargar() {
+
+        //Identificar los textView para despues rellenarlos con los que leamos de la base de datos
+        TextView nombre = (TextView)findViewById(R.id.textView2);
+        TextView nombre_completo = (TextView)findViewById(R.id.textView4);
+        TextView posicion = (TextView)findViewById(R.id.textView6);
+        TextView fecha_nacimiento = (TextView)findViewById(R.id.textView8);
+        TextView nacido = (TextView)findViewById(R.id.textView10);
+        TextView peso = (TextView)findViewById(R.id.textView12);
+        TextView estatura = (TextView)findViewById(R.id.textView14);
+        TextView procedencia = (TextView)findViewById(R.id.textView16);
+        TextView dorsal = (TextView)findViewById(R.id.textView18);
+
+        BaseDatosOpenHelper baseHelper = new BaseDatosOpenHelper(this, "BaseDatosDeportiva", null, 1);
+        SQLiteDatabase db = baseHelper.getReadableDatabase();
+        if (db != null) {
+            Cursor c = db.rawQuery("SELECT * FROM Jugadores WHERE Nombre='Arrizabalaga' AND Dorsal=1", null);
+            int cantidad = c.getCount();//Cantidad de registros
+            int i = 0;
+            // arreglo = new String[cantidad];
+            if (c.moveToFirst()) {
+                do {
+                    nombre.setText(c.getString(1));
+                    nombre_completo.setText(c.getString(2));
+                    posicion.setText(c.getString(3));
+                    fecha_nacimiento.setText(c.getString(4));
+                    nacido.setText(c.getString(5));
+                    peso.setText(c.getString(6));
+                    estatura.setText(c.getString(7));
+                    procedencia.setText(c.getString(8));
+                    dorsal.setText(c.getString(9));
+                    //String linea = c.getInt(0) + " " + c.getString(1) + " " + c.getString(2) + " " + c.getInt(3);
+
+                    //arreglo[i]=linea;
+                    //i++;
+
+                } while (c.moveToNext());
+            }
+
+            // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arreglo);
+            // ListView lista = (ListView)findViewById(R.id.Lista);
+            // lista.setAdapter(adapter);
+        }
+    }
 }
--- app/src/main/java/com/deportiva/test/ApplicationTest.java
+++ app/src/main/java/com/deportiva/test/ApplicationTest.java
@@ -6,6 +6,12 @@ import android.test.ApplicationTestCase;
 
 import junit.framework.Assert;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -14,14 +20,14 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testPermissionWriteExternal() throws Throwable{
+    public void testPermissionWriteExternal(){
         String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
         int res = getContext().checkCallingOrSelfPermission(permission);
         boolean resultado= (res == PackageManager.PERMISSION_GRANTED);
         Assert.assertTrue(resultado);
     }
 
-    public void testPermissionInternet() throws Throwable{
+    public void testPermissionInternet(){
         String permission = "android.permission.INTERNET";
         int res = getContext().checkCallingOrSelfPermission(permission);
         boolean resultado= (res == PackageManager.PERMISSION_GRANTED);
@@ -41,4 +47,95 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         boolean resultado= (res == PackageManager.PERMISSION_GRANTED);
         Assert.assertFalse(resultado);
     }
+
+    /*----------------------------------------
+
+    Hacer tests para las paginas web, los periodicos y la pagina de enlaces.
+    Tests para ver que se inicia la Acivity correcta de los cases
+
+    //-----------------------------------------------*/
+    public void testHttpGetMarcadores() {
+        HttpGet httpget = new HttpGet("http://www.movil.resultados-futbol.com/segunda");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
+    public void testHttpGetClasificacion() {
+        HttpGet httpget = new HttpGet("http://www.marcadores.com/futbol/espana/liga-adelante/clasificacion.html");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
+    public void testHttpGetCalendarioLfp() {
+        HttpGet httpget = new HttpGet("http://www.lfp.es/calendario-horario");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
+    public void testHttpGetCalendarioMarca() {
+        HttpGet httpget = new HttpGet("http://www.marca.com/movil/tabla_marcadores.html?c=futbol_2adivision");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
+    public void testHttpGetCalendarioMundoDeportivo() {
+        HttpGet httpget = new HttpGet("http://www.mundodeportivo.com/futbol/liga-segunda-division/calendario.html");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
+    public void testHttpGetCalendarioElPais() {
+        HttpGet httpget = new HttpGet("http://www.elpais.com/especial/calendario-liga-futbol/segunda/");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        try {
+            response = httpclient.execute(httpget);
+        } catch(Exception e) {
+            fail("Excepcion");
+        }
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode() );
+    }
+
 }
